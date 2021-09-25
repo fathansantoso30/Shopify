435da61dcfd63a3ccfde9eddfbdcc2701603957a
fathansantoso30 <fathansantoso30@gmail.com>
2021-09-25 16:22:11 +0700

    added add item feature and mongodb

--- app.js
+++ app.js
@@ -1,36 +1,61 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Item = require('./models/item');
+// express app
 const app = express();
 
+// connect to mongodb server
+const dbURL = 'mongodb+srv://tester:test1234@shopify1.r1miz.mongodb.net/shopify1?retryWrites=true&w=majority';
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+             
+// middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
-
+// home page
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Home' });
 });
 
+// lists page
 app.get('/lists', (req, res) => {
-    const lists = [
-        {id: 1, name: 'Spinach'},
-        {id: 2, name: 'Tomatoes'},
-        {id: 3, name: 'Cauliflowers'},
-    ];
-    res.render('lists', { lists });
+    Item.find().sort({ createdAt: -1 })
+    .then(result => {
+        res.render('lists', { title: 'Shopping list', items: result});
+    })
+    .catch(err => {
+        console.log(err);
+    });
 });
 
+app.post('/lists', (req, res) => {
+    const item = new Item(req.body);
+
+    item.save()
+        .then(result => {
+            res.redirect('/lists');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+// add page
 app.get('/add', (req, res) => {
-    res.render('add');
+    res.render('add', { title: 'Add item' });
 });
 
+// edit page
 app.get('/edit', (req, res) => {
-    res.render('edit');
+    res.render('edit', { title: 'Edit item' });
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 })
 
